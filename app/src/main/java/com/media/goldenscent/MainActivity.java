57f60cfc8a7e2495281eab20f06f437febe09acb
SamihMustafa <samihmustafa@gmail.com>
2018-06-17 20:05:28 +0400

    refractor code to fragment

--- app/src/main/java/com/media/goldenscent/MainActivity.java
+++ app/src/main/java/com/media/goldenscent/MainActivity.java
@@ -1,83 +1,25 @@
 package com.media.goldenscent;
 
-import android.media.MediaPlayer;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.VideoView;
 
-import com.media.goldenscent.ui.CarouselAdapter;
-import com.media.goldenscent.ui.CarouselLayoutManager;
 
 public class MainActivity extends AppCompatActivity {
 
-    private VideoView myVideo;
-    private RecyclerView carousel;
-    private ImageButton leftImageButton;
-    private ImageButton rightImageButton;
-    private CarouselLayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setVideoPlayer();
+        if (savedInstanceState == null) {
+            MainFragment fragment = new MainFragment();
 
-        setCarouselList();
-
-        setImageButtons();
-
-    }
-
-    private void setImageButtons() {
-        leftImageButton = findViewById(R.id.move_left);
-        rightImageButton = findViewById(R.id.move_right);
-
-        leftImageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int pos = layoutManager.findFirstVisibleItemPosition();
-                carousel.scrollToPosition(pos - 1);
-            }
-        });
-
-
-        rightImageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int pos = layoutManager.findLastVisibleItemPosition();
-                carousel.scrollToPosition(pos + 1);
-            }
-        });
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.content_frame, fragment, MainFragment.TAG).commit();
+        }
 
     }
 
-    private void setCarouselList() {
-        carousel = findViewById(R.id.myCarousel);
-        layoutManager = (new CarouselLayoutManager(this));
-        carousel.setLayoutManager(layoutManager);
-        carousel.setHasFixedSize(true);
 
-        CarouselAdapter adapter = new CarouselAdapter(this);
-        carousel.setAdapter(adapter);
-        carousel.getLayoutManager().scrollToPosition(Integer.MAX_VALUE / 2);
-    }
-
-    private void setVideoPlayer() {
-        myVideo = findViewById(R.id.myVideo);
-        myVideo.setVideoURI(Uri.parse("http://184.72.239.149/vod/smil:BigBuckBunny.smil/playlist.m3u8"));
-        myVideo.start();
-        myVideo.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mediaPlayer) {
-                mediaPlayer.setVolume(0,0);
-                mediaPlayer.setLooping(true);
-            }
-        });
-    }
 }
---
+++ app/src/main/java/com/media/goldenscent/MainFragment.java
@@ -0,0 +1,102 @@
+package com.media.goldenscent;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.VideoView;
+
+import com.media.goldenscent.ui.CarouselAdapter;
+import com.media.goldenscent.ui.CarouselLayoutManager;
+
+/**
+ * Created by Samih on 17-Jun-18.
+ */
+public class MainFragment extends Fragment {
+
+    public static final String TAG = "Main Fragment";
+
+    private VideoView myVideo;
+    private RecyclerView carousel;
+    private ImageButton leftImageButton;
+    private ImageButton rightImageButton;
+    private CarouselLayoutManager layoutManager;
+
+
+
+    public MainFragment(){
+
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        setVideoPlayer(view);
+        setImageButtons(view);
+        setCarouselList(view);
+
+
+
+        return view;
+    }
+
+
+    private void setImageButtons(View view) {
+        leftImageButton = view.findViewById(R.id.move_left);
+        rightImageButton = view.findViewById(R.id.move_right);
+
+        leftImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int pos = layoutManager.findFirstVisibleItemPosition();
+                carousel.scrollToPosition(pos - 1);
+            }
+        });
+
+
+        rightImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int pos = layoutManager.findLastVisibleItemPosition();
+                carousel.scrollToPosition(pos + 1);
+            }
+        });
+
+    }
+
+    private void setCarouselList(View view) {
+        carousel = view.findViewById(R.id.myCarousel);
+        layoutManager = (new CarouselLayoutManager(getContext()));
+        carousel.setLayoutManager(layoutManager);
+        carousel.setHasFixedSize(true);
+
+        CarouselAdapter adapter = new CarouselAdapter(getContext());
+        carousel.setAdapter(adapter);
+        carousel.getLayoutManager().scrollToPosition(Integer.MAX_VALUE / 2);
+    }
+
+    private void setVideoPlayer(View view) {
+        myVideo = view.findViewById(R.id.myVideo);
+        myVideo.setVideoURI(Uri.parse("http://184.72.239.149/vod/smil:BigBuckBunny.smil/playlist.m3u8"));
+        myVideo.start();
+        myVideo.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setVolume(0,0);
+                mediaPlayer.setLooping(true);
+            }
+        });
+    }
+
+}
