fbf07508b905fc359dd7b2cd9b3ba65e185d2f42
SamihMustafa <samihmustafa@gmail.com>
2018-06-17 10:52:15 +0400

    added basic video player

--- app/src/main/java/com/media/goldenscent/MainActivity.java
+++ app/src/main/java/com/media/goldenscent/MainActivity.java
@@ -1,13 +1,29 @@
 package com.media.goldenscent;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private VideoView myVideo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myVideo = findViewById(R.id.myVideo);
+        myVideo.setVideoURI(Uri.parse("http://184.72.239.149/vod/smil:BigBuckBunny.smil/playlist.m3u8"));
+        myVideo.start();
+        myVideo.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setLooping(true);
+            }
+        });
+
     }
 }
